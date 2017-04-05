ba1c37bb3f4110ec26537d40faf540a1fbca0ca6
Reckfu1 <1970709700@qq.com>
2017-04-05 23:28:33 +0800

    udpate

--- src/components/news.vue
+++ src/components/news.vue
@@ -4,31 +4,47 @@
             <div class="content-text">{{item.title}}</div>
             <div class="content-img"><img :src="'http://read.html5.qq.com/image?src=forum&q=5&r=0&imgflag=7&imageUrl='+imgsrc[index]" alt=""></div>
         </div>
+        <div id="old-news">
+            <div id="yesterday-news">{{getYesterday}}</div>
+<!--             <div id="yesterday-news-items" v-for="(item,index) in yesterday_news">
+                <div class="content-text"></div>
+                <div class="content-img"><img :src="" alt=""></div>
+            </div> -->
+        </div>
     </div>
 </template>
 
 
 
 
+
+
 <script>
-// import content from './content.vue'
 
 export default {
 
     data(){
         return {
             news:[],
-            imgsrc:[]
+            imgsrc:[],
+            yesterday_news:[]
         }
     },
 
     created(){
-        let _this=this;
+        let _this=this
+        let date=new Date()
+        let year,month,day
+        let str=''
+        year=date.getFullYear()
+        month=date.getMonth()+1
+        day=date.getDate()
+        str=''+year+month+day
         this.$http.get('/api/4/news/latest')
             .then((res) => {
                 _this.news=res.data.stories
                 for(let i=0;i<_this.news.length;i++){
-                    _this.imgsrc[i]=_this.news[i].images;
+                    _this.imgsrc[i]=_this.news[i].images
                 }
             })
             .catch((res) => {
@@ -36,6 +52,48 @@ export default {
                     console.log('Error',res.message)
                 }
             })
+        if(str>='2013520'){
+            if(month<10){
+                month='0'+month;
+            }
+            if(day<10){
+                day='0'+day;
+            }
+        }
+    },
+    computed:{
+        getYesterday(){
+            let date= new Date()
+            let month,day,what_day
+            let str=''
+            month=date.getMonth()+1
+            day=date.getDate()-1
+            what_day=date.getDay()
+            switch(what_day){
+                case 0:
+                    str='六'
+                    break
+                case 1:
+                    str='日'
+                    break
+                case 2:
+                    str='一'
+                    break
+                case 3:
+                    str='二'
+                    break
+                case 4:
+                    str='三'
+                    break
+                case 5:
+                    str='四'
+                    break
+                case 6:
+                    str='五'
+                    break
+            }
+            return month+'月'+day+'日'+' '+'星期'+str
+        }
     }
 }
 
@@ -59,14 +117,26 @@ export default {
     /*background-color: #fff;*/
     font-weight: bolder;
     font-size: 14px;
+    margin-top:-4px;
 }
 .content-img{
     height: 65px;
-    width:25%;
+    width:24%;
     /*background-color: #fbb;*/
 }
 .content-img img{
     height: 100%;
     width: 100%;
+}
+/*old news*/
+#yesterday-news{
+    width:100%;
+    height: 30px;
+    background-color: #323232;
+    font-size: 14px;
+    color:#fff;
+    text-align: center;
+    line-height: 30px;
+    letter-spacing: 2px;
 }
 </style>
